ed1744ab4fb19ce3e67c2fcee274d5d680ac2154
JoseCorellaVega <jose.corella.vega@udla.edu.ec>
2025-05-01 17:24:29 -0500

    commit con masccotas/especies3

--- src/GestionM.java
+++ src/GestionM.java
@@ -1,2 +1,10 @@
 public class GestionM {
+
+    public final static int cantidadboac = 25;
+    public final static int cantidadcacatua = 20;
+    public final static int cantidadtitidorado = 20;
+    public final static int cantidadguacaroja = 12;
+
+
+
 }
