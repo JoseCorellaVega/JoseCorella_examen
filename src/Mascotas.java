025e9735d68c2fe1df02591ce11b242e988b4016
JoseCorellaVega <jose.corella.vega@udla.edu.ec>
2025-05-01 17:07:14 -0500

    commit con masccotas/especies

---
+++ src/Mascotas.java
@@ -0,0 +1,72 @@
+public class Mascotas {
+    public class Juego
+    {
+
+        public enum Especie
+        {
+            CacatuaBlanca,
+            BoaConstrictor,
+            TitiDorado,
+            GuacamayaRoja
+        }
+
+        private Especie Especie;
+
+        private int vidapromedio;
+
+        private int precio;
+
+        private int cantidadespecies;
+
+        private int cantidadVendidas;
+
+        private int tamano;
+
+        public Mascota( Especie pespecie, int ptamanio, int pPrecio, int pVidaPromedio , int pCantidadEspecies, int pCantidadVendidas)
+        {
+            Especie = pespecie;
+            precio = pPrecio;
+            vidapromedio = pVidaPromedio;
+            cantidadespecies = pCantidadEspecies;
+            cantidadVendidas = 0;
+            tamano = ptamanio;
+
+        }
+
+        public Especie Especie( )
+        {
+            return Especie;
+        }
+
+        public darVidaPromediodar( )
+        {
+            return vidapromedio;
+        }
+
+
+        public int darPrecio( )
+        {
+            return precio;
+        }
+
+        public int dar tamano( )
+        {
+            return tamano;
+        }
+        }
+    }
+        public int darCantidadespecies( )
+        {
+            return cantidadespecies;
+        }
+
+
+        public int darCantidadVendidas( )
+        {
+            return cantidadVendidas;
+        }
+
+
+
+
+
